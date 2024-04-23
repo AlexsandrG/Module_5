a5eea00b3c3775cd764476bc22a737038c771e95
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-04-23 17:49:00 +0300

    Add files via upload

---
+++ Peregruzka.py
@@ -0,0 +1,9 @@
+# Создайте новый класс Buiding
+# Создайте инициализатор для класса Buiding, который будет задавать целочисленный атрибут этажности self.numberOfFloors
+# и строковый атрибут self.buildingType
+# Создайте(перегрузите) __eq__, используйте атрибут numberOfFloors и buildingType для сравнения
+
+class Bulding:
+    def __init__(self):
+        self.numberOfFloors = int()
+        self.buildingType = str()
