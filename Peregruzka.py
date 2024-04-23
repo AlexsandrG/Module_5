1012121d3be836b4cf4882e9b218fc6082b76d7a
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-04-23 17:53:14 +0300

    Add files via upload

--- Peregruzka.py
+++ Peregruzka.py
@@ -3,7 +3,19 @@
 # и строковый атрибут self.buildingType
 # Создайте(перегрузите) __eq__, используйте атрибут numberOfFloors и buildingType для сравнения
 
-class Bulding:
-    def __init__(self):
-        self.numberOfFloors = int()
-        self.buildingType = str()
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
+
+# Пример использования класса:
+building1 = Building(5, "office")
+building2 = Building(5, "office")
+building3 = Building(10, "residential")
+
+# Проверка равенства
+print(building1 == building2)  # Ожидаем получить True
+print(building1 == building3)  # Ожидаем получить False
