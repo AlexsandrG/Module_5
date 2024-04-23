91975f685d539c9da1d0e92ac00fecf5463e3b7d
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-04-23 19:27:09 +0300

    Add files via upload

---
+++ Building total.py
@@ -0,0 +1,15 @@
+# Создайте новый класс Buiding с атрибутом total
+# Создайте инициализатор для класса Buiding, который будет увеличивать атрибут количества созданных объектов
+# класса Building total (по примеру класса Lemming из урока)
+# В цикле создайте 40 объектов класса Building и выведите их на экран командой print
+
+class Building:
+    def __init__(self):
+        self.total = 40
+
+    def Building_total(self):
+        print(self.total)
+
+
+building = Building()
+# print()
